6e1586a88aee0472d8ab3478c42f0e68c33a2b43
Vy <tuongvy.tran@gmail.com>
2022-11-13 15:05:21 -0600

    practice1

---
+++ main.js
@@ -0,0 +1,24 @@
+console.log('Hello World');
+/* Date Types:
+underfined, null, boolean (true or false), string ( text), number and object*/
+
+//var myName = 'Vy'  //can be used the whole programme
+
+//myName = 8
+
+//let ourName = 'freeCodeCamp'   // only can be used within the scope where declared
+
+//const pi = 3.14  // can never change
+
+//var a;  // declare a variable
+//var b = 2;   // declare and assign the value
+//a = 7;
+//b = a;
+
+var sum = 10 + 10;
+console.log(sum);
+
+var myVar = 88;
+//myVar = myVar + 1;
+myVar++;
+console.log(myVar);
